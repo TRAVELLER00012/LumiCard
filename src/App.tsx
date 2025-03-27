98991013b36bec9fc09dcece41c8d4ebe799cfa6
Daksh Kumar <k.daksh29@gmail.com>
2025-03-28 04:43:12 +0530

    add particles bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Navbar from "./components/Navbar/navbar"
+import ParticleBackground from "./components/Particles/particles"
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <ParticleBackground />
     </>
   )
 }
---
+++ src/components/Particles/particles.tsx
@@ -0,0 +1,133 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type ISourceOptions,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+
+const ParticleBackground = () => {
+    const [, setInit] = useState(false);
+
+    useEffect(() => {
+      initParticlesEngine(async (engine) => {
+        await loadSlim(engine);
+      }).then(() => {
+        setInit(true);
+      });
+    }, []);
+  
+    const particlesLoaded = async (): Promise<void> => {};
+  
+    const options: ISourceOptions = useMemo(
+      () => ({
+        particles: {
+          
+          number: {
+            value: 100,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            /*
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.75,
+            width: 1.5,
+            */
+           enable:false
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 10,
+            }
+          },
+          opacity: {
+            value: 0.6,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 5,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 25,
+              size_min: 0.1,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 2.5,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        retina_detect: true,
+      }),
+      [],
+    );
+  return (
+    <Particles
+      id="tsparticles"
+      particlesLoaded={particlesLoaded}
+      options={options}
+      className="bg-particles" // Apply the background class here
+    />
+  )
+}
+
+export default ParticleBackground
