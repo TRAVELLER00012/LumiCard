0fca092da57444a516fecfdaa7d20d9407cb1ef9
Daksh Kumar <k.daksh29@gmail.com>
2025-04-03 16:16:11 +0530

    refractor stars

--- src/hooks/useStars.tsx
+++ src/hooks/useStars.tsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react"
 import star_styles from "../css/stars.module.css"
 
-const useStars = (styles:CSSModuleClasses = star_styles,blueStarsCount: number = 5, redStarsCount : number = 5) =>{
-    const [blueStars,setBlueStars] = useState<{tr:JSX.Element[],bl:JSX.Element[]}>({bl:[],tr:[]})
-    const [redStars,setRedStars] = useState<{tr:JSX.Element[],bl:JSX.Element[]}>({bl:[],tr:[]})
+const useStars = (styles:CSSModuleClasses = star_styles,star1Count: number = 5, star2Count : number = 5) =>{
+    const [star1,setStar1] = useState<{tr:JSX.Element[],bl:JSX.Element[]}>({bl:[],tr:[]})
+    const [star2,setStar2] = useState<{tr:JSX.Element[],bl:JSX.Element[]}>({bl:[],tr:[]})
 
     useEffect(() =>{
-        const b_tr_stars = genStars("TR", "BLUE",blueStarsCount,styles)
-        const r_tr_stars = genStars("TR", "RED",redStarsCount,styles)
+        const b_tr_stars = genStars("TR", "STAR1",star1Count,styles)
+        const r_tr_stars = genStars("TR", "STAR2",star2Count,styles)
         
-        const b_bl_stars = genStars("BL", "BLUE",blueStarsCount,styles)
-        const r_bl_stars = genStars("BL", "RED",redStarsCount,styles)
+        const b_bl_stars = genStars("BL", "STAR1",star1Count,styles)
+        const r_bl_stars = genStars("BL", "STAR2",star2Count,styles)
     
-        setBlueStars({tr:b_tr_stars,bl:b_bl_stars})
-        setRedStars({tr:r_tr_stars,bl:r_bl_stars})
+        setStar1({tr:b_tr_stars,bl:b_bl_stars})
+        setStar2({tr:r_tr_stars,bl:r_bl_stars})
     },[])
 
-    return [blueStars,redStars]
+    return [star1,star2]
 }
 
-const genStars = (position : "TR" | "BL", type : "BLUE" | "RED", count : number , styles : CSSModuleClasses) =>{
+const genStars = (position : "TR" | "BL", type : "STAR1" | "STAR2", count : number , styles : CSSModuleClasses) =>{
     const stars: JSX.Element[] = []
     if(position ==  "TR"){
         for (let i = 0; i < count; i++){
@@ -27,8 +27,8 @@ const genStars = (position : "TR" | "BL", type : "BLUE" | "RED", count : number 
             const randTop =  Math.floor(Math.random() * (25 -5 +1)) +5;
             const randSpeed = Math.floor(Math.random() * (10-5+1) + 5)
             let star = <div></div>;
-            if(type == "BLUE") star = <div className={[styles.star,styles.starTR,styles.blueStar,`star${i}`].join(" ")} style={{top:`${randTop}%`,right:`${randRight}%`,animationDuration:`${randSpeed}s`}}></div>
-            else star = <div className={[styles.star,styles.starTR,styles.redStar,`star${i}`].join(" ")} style={{top:`${randTop}%`,right:`${randRight}%`,animationDuration:`${randSpeed}s`}}></div>
+            if(type == "STAR1") star = <div className={[styles.star,styles.starTR,styles.star1,`star${i}`].join(" ")} style={{top:`${randTop}%`,right:`${randRight}%`,animationDuration:`${randSpeed}s`}}></div>
+            else star = <div className={[styles.star,styles.starTR,styles.star2,`star${i}`].join(" ")} style={{top:`${randTop}%`,right:`${randRight}%`,animationDuration:`${randSpeed}s`}}></div>
             stars.push(star)
         }
 
@@ -39,8 +39,8 @@ const genStars = (position : "TR" | "BL", type : "BLUE" | "RED", count : number 
             const randBottom =  Math.floor(Math.random() * (25 -5 +1)) +5;
             const randSpeed = Math.floor(Math.random() * (10-5+1) + 5)
             let star = <div></div>;
-            if(type == "BLUE") star = <div className={[styles.star,styles.starBL,styles.blueStar,`star${i}`].join(" ")} style={{bottom:`${randBottom}%`,left:`${randLeft}%`,animationDuration:`${randSpeed}s`}}></div>
-            else star = <div className={[styles.star,styles.starBL,styles.redStar,`star${i}`].join(" ")} style={{bottom:`${randBottom}%`,left:`${randLeft}%`,animationDuration:`${randSpeed}s`}}></div>
+            if(type == "STAR1") star = <div className={[styles.star,styles.starBL,styles.star1,`star${i}`].join(" ")} style={{bottom:`${randBottom}%`,left:`${randLeft}%`,animationDuration:`${randSpeed}s`}}></div>
+            else star = <div className={[styles.star,styles.starBL,styles.star2,`star${i}`].join(" ")} style={{bottom:`${randBottom}%`,left:`${randLeft}%`,animationDuration:`${randSpeed}s`}}></div>
             stars.push(star)
         }
     }
